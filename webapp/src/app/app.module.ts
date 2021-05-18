9e15ad9ee170cb76f65cf88492fed7639369e071
surya <suryansh.garg234@gmail.com>
2021-05-18 19:25:28 +0530

    commited tasks

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { UploadchallengeComponent } from './components/uploadchallenge/uploadchallenge.component';
+import {   FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadchallengeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
