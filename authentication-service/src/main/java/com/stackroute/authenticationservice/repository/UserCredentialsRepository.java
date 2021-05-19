83534a90ed9bab2db3a69a66f1dce66fd32eba06
varaganti939 <varaganti939@gmail.com>
2021-05-19 13:20:21 +0530

    JWT Token generation

--- authentication-service/src/main/java/com/stackroute/authenticationservice/repository/UserCredentialsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.stackroute.authenticationservice.repository;
-
-import com.stackroute.authenticationservice.model.UserCredentials;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserCredentialsRepository extends CrudRepository<UserCredentials,Integer> {
-}
