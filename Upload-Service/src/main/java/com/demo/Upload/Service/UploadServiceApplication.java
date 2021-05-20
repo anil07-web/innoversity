bf220b13e0f5f58499ec289ede6df2ebc3656358
surya <suryansh.garg234@gmail.com>
2021-05-20 17:16:47 +0530

    upload-service

---
+++ Upload-Service/src/main/java/com/demo/Upload/Service/UploadServiceApplication.java
@@ -0,0 +1,15 @@
+package com.demo.Upload.Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadServiceApplication  {
+
+
+public static void main(String[] args) {
+    SpringApplication.run(UploadServiceApplication.class, args);
+}
+
+
+}
