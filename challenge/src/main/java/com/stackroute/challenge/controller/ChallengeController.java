f9844635fdc4f54394bc282798732396243b6281
anilkumar75001900 <anilkumar75001900@gmail.com>
2021-05-26 13:20:25 +0530

    passed data challenge-service to search-service

--- challenge/src/main/java/com/stackroute/challenge/controller/ChallengeController.java
+++ challenge/src/main/java/com/stackroute/challenge/controller/ChallengeController.java
@@ -2,6 +2,7 @@ package com.stackroute.challenge.controller;
 
 import com.stackroute.challenge.model.Challenge;
 import com.stackroute.challenge.service.ChallengeService;
+import com.stackroute.challenge.service.RabbitMqSender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,12 @@ public class ChallengeController {
 
     @Autowired
     ChallengeService challengeService;
+    RabbitMqSender rabbitMqSender;
+    public ChallengeController(RabbitMqSender rabbitMqSender){
+        this.rabbitMqSender=rabbitMqSender;
+    }
+
+
 
     @PostMapping("/Challenge")
     public ResponseEntity<Challenge> saveChallenge(@RequestBody Challenge challenge)
@@ -24,6 +31,7 @@ public class ChallengeController {
         UUID uuid=UUID.randomUUID();
         challenge.setChallengeId(uuid);
         Challenge savedChallenge=challengeService.save(challenge);
+        rabbitMqSender.send(challenge);
         return new ResponseEntity<>(savedChallenge, HttpStatus.CREATED);
     }
 }
