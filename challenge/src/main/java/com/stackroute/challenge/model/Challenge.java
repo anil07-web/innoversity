34a83aff21626f541fb8a0af4eebe05d942bb4ed
surya <suryansh.garg234@gmail.com>
2021-05-26 20:29:23 +0530

    updated

--- challenge/src/main/java/com/stackroute/challenge/model/Challenge.java
+++ challenge/src/main/java/com/stackroute/challenge/model/Challenge.java
@@ -7,6 +7,8 @@ import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.UUID;
 
 @Data
@@ -23,4 +25,9 @@ public class Challenge {
     private String challengeAbstract; //I make capital (Abstract) due to abstract keyword predefined in java
     private String description;
     private String rules;
+
+    LocalDate localDate = LocalDate.now(); // used in java 8
+    Date date =new Date();
+
+
 }
