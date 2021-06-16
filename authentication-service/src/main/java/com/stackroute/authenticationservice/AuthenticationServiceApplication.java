b4405e7fe178c20544f988f009b64f370ca74d6b
varaganti939 <varaganti939@gmail.com>
2021-06-16 09:45:51 +0530

    Pulling the changes

--- authentication-service/src/main/java/com/stackroute/authenticationservice/AuthenticationServiceApplication.java
+++ authentication-service/src/main/java/com/stackroute/authenticationservice/AuthenticationServiceApplication.java
@@ -4,6 +4,7 @@ package com.stackroute.authenticationservice;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableRabbit
