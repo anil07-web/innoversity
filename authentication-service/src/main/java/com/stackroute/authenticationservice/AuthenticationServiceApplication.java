e178e19053d537d9769c42e530b5124cce0fc0a6
varaganti939 <varaganti939@gmail.com>
2021-06-16 14:45:43 +0530

    Implemented Eureka Server

--- authentication-service/src/main/java/com/stackroute/authenticationservice/AuthenticationServiceApplication.java
+++ authentication-service/src/main/java/com/stackroute/authenticationservice/AuthenticationServiceApplication.java
@@ -4,8 +4,10 @@ package com.stackroute.authenticationservice;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableRabbit
 public class AuthenticationServiceApplication{
--- search-service/src/main/java/com/stackroute/searchservice/SearchServiceApplication.java
+++ search-service/src/main/java/com/stackroute/searchservice/SearchServiceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableEurekaClient
 @EnableAutoConfiguration
 @EnableRabbit
 @SpringBootApplication
